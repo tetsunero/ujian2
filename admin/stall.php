d47e596f2666c2bb82e1dff4894784f413e36215
tetsunero <dedy.iswana2@gmail.com>
2022-05-24 23:36:54 +0700

    Delete admin directory

--- admin/stall.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$files = glob("/home/admincbt/ujian2/admin/ujian2/admin/*.*");
-foreach($files as $file){
-$file_to_go = str_replace("/home/admincbt/ujian2/admin/ujian2/admin/","/home/admincbt/ujian2/admin/",$file);
-copy($file, $file_to_go);
-}	   		
-?>
